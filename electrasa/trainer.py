3f6a3d7bd0acb9d27cae17edbcc50358542f734d
Ubuntu <ltech@rasalt-dev.p1nvgwc5saqe3ass43yrm55tjc.syx.internal.cloudapp.net>
2020-06-25 12:08:19 +0000

    apply dynamic batch auto scaling mode

--- electrasa/trainer.py
+++ electrasa/trainer.py
@@ -16,20 +16,28 @@ def train(
     intent_optimizer_lr=2e-4,
     entity_optimizer_lr=8e-4,
     epochs=20,
+    batch_size=None,
     gpu_num=1,
     o_tag_class_weight=0.05,  # O tag class weight for training
     distributed_backend=None,
 ):
-    trainer = Trainer(
-        auto_scale_batch_size="binsearch",
-        max_epochs=epochs,
-        gpus=gpu_num,
-        distributed_backend=distributed_backend,
-    )
+    if batch_size is None:
+        trainer = Trainer(
+            auto_scale_batch_size="binsearch",
+            max_epochs=epochs,
+            gpus=gpu_num,
+            distributed_backend=distributed_backend,
+        )
+    else:
+        trainer = Trainer(
+            max_epochs=epochs,
+            gpus=gpu_num,
+            distributed_backend=distributed_backend,
+        )
 
     model_args = {}
     model_args["epochs"] = epochs
-    model_args["batch_size"] = 128 # search optimial batch size from default small value
+    model_args["batch_size"] = batch_size
     model_args["nlu_data"] = open(file_path, encoding="utf-8").readlines()
     model_args["train_ratio"] = train_ratio
     model_args["optimizer"] = optimizer
