797d8588aad89029c292ea4d619a01e52caae86e
cheesama <cheehoon12@hanmail.net>
2020-07-01 18:36:00 +0900

    set variable def in tf file

--- azure-vm.tf
+++ azure-vm.tf
@@ -1,3 +1,16 @@
+variable "az_subscription_id" {
+  description = "The Subscription ID for the Service Principal to use for AKS"
+}
+variable "az_client_id" {
+  description = "The Client ID for the Service Principal to use for AKS"
+}
+variable "az_client_secret" {
+  description = "The Client Secret for the Service Principal to use for AKS"
+}
+variable "az_tenant_id" {
+  description = "The Tenant ID for the Service Principal to use for AKS"
+}
+
 locals {
     tags = {
         created-by  = "Terraform"
