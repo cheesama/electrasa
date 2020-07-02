bd145bc79f9b2db449704f552f2ce392c93099eb
cheesama <cheehoon12@hanmail.net>
2020-07-02 17:26:55 +0900

    modify test training code intent learning rate

--- tests/test_KoELECTRA_backbone.py
+++ tests/test_KoELECTRA_backbone.py
@@ -6,7 +6,7 @@ import os, sys
 trainer.train(
     file_path= expanduser("~") + os.sep + "nlu.md",
     batch_size=128,
-    intent_optimizer_lr=2e-4,
+    intent_optimizer_lr=4e-4,
     entity_optimizer_lr=4e-4,
     gpu_num=0,
 )
