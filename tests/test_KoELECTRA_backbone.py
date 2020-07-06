9c0e000f1eb4519c91f39e6516500f4ca3e05eda
Ubuntu <ltech@rasalt-dev.p1nvgwc5saqe3ass43yrm55tjc.syx.internal.cloudapp.net>
2020-07-06 01:35:06 +0000

    modify nlu data download path

--- tests/test_KoELECTRA_backbone.py
+++ tests/test_KoELECTRA_backbone.py
@@ -4,7 +4,7 @@ from electrasa import trainer
 import os, sys
 
 trainer.train(
-    file_path= expanduser("~") + os.sep + "nlu.md",
+    file_path="nlu.md",
     batch_size=128,
     intent_optimizer_lr=4e-4,
     entity_optimizer_lr=4e-4,
